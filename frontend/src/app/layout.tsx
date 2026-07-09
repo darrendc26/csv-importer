6411678e62797af8d7f716c79263af57ed934867
Darren <darren26feb@gmail.com>
2026-07-09 12:33:41 +0530

    fixes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata = {
@@ -13,7 +14,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XEDZJ7Q5WV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-XEDZJ7Q5WV');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
 }
+
